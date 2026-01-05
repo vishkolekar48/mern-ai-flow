d4aaffe8fbc1ed60a6b165b3616011262c5be4b2
vishkolekar48 <kolekarv821@gmail.com>
2026-01-05 13:07:22 +0530

    update Constant

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,5 @@
-const CURRENT_API_URL = 'http://localhost:5000/api'
+// const CURRENT_API_URL = 'http://localhost:5000/api'
+const CURRENT_API_URL = 'https://mern-ai-flow.onrender.com/api'
 
 export {
     CURRENT_API_URL
